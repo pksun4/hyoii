651eb34c2e81d36f7732d3c2869b527a89db7b4f
hyo <pksun4@gmail.com>
2023-11-12 21:57:52 +0900

    apply .gitignore

--- common/src/main/kotlin/com/hyoii/core/domain/brand/BrandRepository.kt
+++
@@ -1,7 +0,0 @@
-package com.hyoii.core.domain.brand
-
-import org.springframework.data.jpa.repository.JpaRepository
-import org.springframework.stereotype.Repository
-
-@Repository
-interface BrandRepository : JpaRepository<Brand, Long>
--- mall/src/main/kotlin/com/hyoii/mall/exception/InvalidInputException.kt
+++
@@ -1,6 +0,0 @@
-package com.hyoii.mall.exception
-
-class InvalidInputException(
-    val fieldName: String = "",
-    override val message: String = "Invalid Input"
-) : RuntimeException(message)
