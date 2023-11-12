902742d57b4db64c7bb0f9a4513eaf6db497a885
hyo <pksun4@gmail.com>
2023-11-12 22:33:51 +0900

    update response structure

--- mall/src/main/kotlin/com/hyoii/mall/common/res/ResponseData.kt
+++ mall/src/main/kotlin/com/hyoii/mall/common/res/ResponseData.kt
@@ -5,9 +5,11 @@ import org.springframework.http.ResponseEntity
 
 object ResponseData {
     fun fail(messageEnums: MessageEnums) = ResponseEntity.badRequest().body(ResponseFail(messageEnums.code, messageEnums.message))
-    fun <T> success(response: T): ResponseEntity<ResponseSuccess<T>> = ResponseEntity.ok(ResponseSuccess(response))
+    fun <T> success(response: T): ResponseEntity<ResponseSuccess<T>> = ResponseEntity.ok(ResponseSuccess(response, MessageEnums.SUCCESS.code, MessageEnums.SUCCESS.message))
 }
 
-class ResponseSuccess<T>(val data: T)
+class ResponseSuccess<T>(val data: T, val code: String, val message: String)
 
 class ResponseFail(val errorCode: String, val errorMessage: String)
+
+class ResponseException<T> (val errorCode: String = MessageEnums.ERROR.code, val errorMessage: String = MessageEnums.ERROR.message, val error: T? = null)
--- mall/src/main/kotlin/com/hyoii/mall/exception/CustomExceptionHandler.kt
+++ mall/src/main/kotlin/com/hyoii/mall/exception/CustomExceptionHandler.kt
@@ -1,11 +1,11 @@
 package com.hyoii.mall.exception
 
 
+//import org.springframework.security.authentication.BadCredentialsException
 import com.hyoii.core.enums.MessageEnums
-import com.hyoii.mall.common.res.BaseResponse
+import com.hyoii.mall.common.res.ResponseException
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
-//import org.springframework.security.authentication.BadCredentialsException
 import org.springframework.validation.FieldError
 import org.springframework.web.bind.MethodArgumentNotValidException
 import org.springframework.web.bind.annotation.ExceptionHandler
@@ -14,45 +14,44 @@ import org.springframework.web.bind.annotation.RestControllerAdvice
 @RestControllerAdvice
 class CustomExceptionHandler {
 
-
     /**
      * DTO 에 @Valid 과정 중 문제가 발생하면 떨어뜨리는 에러
      */
     @ExceptionHandler(MethodArgumentNotValidException::class)
-    protected fun methodArgumentNotValidException(ex: MethodArgumentNotValidException) : ResponseEntity<BaseResponse<Map<String, String>>> {
+    protected fun methodArgumentNotValidException(ex: MethodArgumentNotValidException) : ResponseEntity<ResponseException<Map<String, String>>> {
         val errors = mutableMapOf<String, String>()
         ex.bindingResult.allErrors.forEach { error ->
             val fieldName = (error as FieldError).field
             val errorMessage = error.defaultMessage
             errors[fieldName] = errorMessage ?: "Not Exception Message"
         }
-        return ResponseEntity(BaseResponse(MessageEnums.INVALID_PARAMETER.code, MessageEnums.INVALID_PARAMETER.message, errors), HttpStatus.BAD_REQUEST)
+        return ResponseEntity(ResponseException(MessageEnums.INVALID_PARAMETER.code, MessageEnums.INVALID_PARAMETER.message, errors), HttpStatus.BAD_REQUEST)
     }
 
     /**
      * added InvalidInputException
      */
     @ExceptionHandler(InvalidInputException::class)
-    protected fun invalidInputException(ex: InvalidInputException) : ResponseEntity<BaseResponse<Map<String, String>>> {
+    protected fun invalidInputException(ex: InvalidInputException) : ResponseEntity<ResponseException<Map<String, String>>> {
         val errors = mapOf(ex.fieldName to (ex.message ?: "Not Exception Message"))
-        return ResponseEntity(BaseResponse(MessageEnums.INVALID_PARAMETER.code, MessageEnums.INVALID_PARAMETER.message, errors), HttpStatus.BAD_REQUEST)
+        return ResponseEntity(ResponseException(MessageEnums.INVALID_PARAMETER.code, MessageEnums.INVALID_PARAMETER.message, errors), HttpStatus.BAD_REQUEST)
     }
 
 //    /**
 //     * 시큐리티 추가 (아이디 비밀번호 검증 실패)
 //     */
 //    @ExceptionHandler(BadCredentialsException::class)
-//    protected fun badCredentialsException(ex: InvalidInputException) : ResponseEntity<BaseResponse<Map<String, String>>> {
+//    protected fun badCredentialsException(ex: InvalidInputException) : ResponseEntity<ResponseException<Map<String, String>>> {
 //        val errors = mapOf(ex.fieldName to (ex.message ?: "Not Exception Message"))
-//        return ResponseEntity(BaseResponse(MessageEnums.LOGIN_INCORRECT.code, MessageEnums.LOGIN_INCORRECT.message, errors), HttpStatus.BAD_REQUEST)
+//        return ResponseEntity(ResponseException(MessageEnums.LOGIN_INCORRECT.code, MessageEnums.LOGIN_INCORRECT.message, errors), HttpStatus.BAD_REQUEST)
 //    }
 
     /**
      * 나머지 exception
      */
     @ExceptionHandler(Exception::class)
-    protected fun defaultException(ex: Exception) : ResponseEntity<BaseResponse<Map<String, String>>> {
+    protected fun defaultException(ex: Exception) : ResponseEntity<ResponseException<Map<String, String>>> {
         val errors = mapOf("cause" to (ex.message ?: "Not Exception Message"))
-        return ResponseEntity(BaseResponse(MessageEnums.FAIL.code, MessageEnums.FAIL.message, errors), HttpStatus.BAD_REQUEST)
+        return ResponseEntity(ResponseException(MessageEnums.FAIL.code, MessageEnums.FAIL.message, errors), HttpStatus.BAD_REQUEST)
     }
 }
